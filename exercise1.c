159e779be5275e354be002539748fb27d17e93e7
JorgeMauricioChan <78285401+JorgeMauricioChan@users.noreply.github.com>
2021-02-13 16:46:22 -0600

    Add files via upload

---
+++ exercise1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+int main(void){
+printf("Jorge Mauricio Chan\n");
+printf("CACJ000915HYNHNRA3\n");
+printf("September 15 2000");
+
+	
+	
+	
+	
+	
+	return 0;
+}
---
+++ exercise5.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void){
+	
+	int integer = 3;
+	float floating = 3.5;
+	char character = "E";
+	
+	printf("The number is %f",integer);
+	printf("The number is %d",floating);
+	printf("The character is: %d",character);
+	
+	
+	return 0;
+}
